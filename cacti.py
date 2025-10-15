0542fdd42d2632861c28a06e2b2d5c81c022478b
Jonathan Carpio <101604637+jona70x@users.noreply.github.com>
2025-10-14 18:01:18 -0700

    turning cacti into a decorator

--- cacti.py
+++ cacti.py
@@ -1,31 +1,37 @@
 def cacti_number(plot):
+    def wrapper():
+        #checking for empty arrays
+        if not plot or not plot[0]:
+            return 0
+        
+        rows = len(plot)
+        cols = len(plot[0])
+        count = 0
 
-   
-    #checking for empty arrays
-    if not plot or not plot[0]:
-        return 0
-    
-    rows = len(plot)
-    cols = len(plot[0])
-    count = 0
+        # iterate rows and columns
 
-    # iterate rows and columns
-
-    for i in range(rows):
-        for j in range(cols):
-            # use a guard to check for safe spots
-            if plot[i][j] != 0:
-                continue
-                
-            # block if any 4-neighbor has a cactus
-            if (i > 0 and plot[i-1][j] == 1) or \
-               (i < rows - 1 and plot[i+1][j] == 1) or \
-               (j > 0 and plot[i][j-1] == 1) or \
-               (j < cols - 1 and plot[i][j+1] == 1):
-                continue
-
-            # plant and count
-            plot[i][j] = 1
-            count += 1
- 
-    return count
+        for i in range(rows):
+            for j in range(cols):
+                # use a guard to check for safe spots
+                if plot[i][j] != 0:
+                    is_safe = True
+                    
+                    #checking if a cacti is planted up, right, down, and left
+                    if i > 0 and plot[i-1][j] == 1:
+                        is_safe = False
+                    # cheking down
+                    if i < rows - 1 and plot[i+1][j] == 1:
+                        is_safe = False
+                    # checking left
+                    if j > 0 and plot[i][j-1] == 1:
+                        is_safe = False
+                    # ckcking right
+                    if j < cols - 1 and plot[i][j+1] == 1:
+                        is_safe = False
+                    # plant cactus if it is safe
+                    if is_safe:
+                        plot[i][j] = 1
+                        #increment cactus count
+                        count += 1
+                    return count
+    return wrapper
